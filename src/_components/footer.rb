a3a55eca250c9c1f91428f5297071fa283750250
Dylan McClain <dylan.mcclain@gmail.com>
2021-07-31 13:53:35 -0700

    basic footer

--- src/_components/footer.rb
+++ src/_components/footer.rb
@@ -1,14 +1,5 @@
-class Footer
+class Footer < Bridgetown::Component
   def initialize
     @site = Bridgetown::Current.site
   end
-
-  def render_in(view_context)
-    <<~HTML
-      <footer>
-        <p>#{@site.metadata[:footer]}</p>
-      </footer>
-    HTML
-  end
 end
-
